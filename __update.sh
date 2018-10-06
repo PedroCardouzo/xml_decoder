e815e225ad5ce30a2ad4f113548a1f75f5dbdab0
Pedro Cardouzo <pedro.cardouzo@gmail.com>
2018-10-05 23:33:07 -0300

    change in __update script

--- __update.sh
+++ __update.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+while getopts 'd' OPT; do
+  case "$OPT" in
+    'd') DEV='true';
+		 echo 'dev mode (.git not deleted)';
+       ;;
+    '?') echo "illegal option @ ""$0" >&2
+       exit -1
+       ;;
+  esac
+done
+
 git pull origin master
+if $DEV != 'true'; then
+	rm -R .git
+fi
